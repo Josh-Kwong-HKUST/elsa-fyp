dfe0431a801a02aed39c6e5a85339d04b441ff9d
josh <wfkwong@connect.ust.hk>
2025-09-26 16:17:35 +0800

    better project structure

---
+++ client/src/client.cpp
@@ -0,0 +1,9 @@
+//
+// Created by josh on 9/26/25.
+//
+
+#include "../client/utils.h"
+
+int main() {
+    hello_world();
+}
---
+++ client/src/utils.cpp
@@ -0,0 +1,10 @@
+//
+// Created by josh on 9/26/25.
+//
+
+#include "../client/utils.h"
+#include <iostream>
+
+void hello_world() {
+    std::cout << "Hello World!" << std::endl;
+}
