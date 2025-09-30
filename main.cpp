0e33256904a6d25834daaba337a5400364e94db7
josh <wfkwong@connect.ust.hk>
2025-09-30 22:53:33 +0800

    fix: cmake build test

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    auto lang = "ELSA FYP Project";
-    std::cout << "Hello and welcome to " << lang << std::endl;
-
-    return 0;
-}
---
+++ scripts/build_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cmake -B build-test -DBUILD_TESTS=ON
+cmake --build build-test
